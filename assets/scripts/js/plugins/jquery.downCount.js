02531c62556a2642bedbd5620eae5cfee07fb0a7
alex-braun <braunacb@gmail.com>
2016-08-30 14:50:44 -0400

    Added bootstrap template.
    JS and css files running.  I will be customizing.

---
+++ assets/scripts/js/plugins/jquery.downCount.js
@@ -0,0 +1,123 @@
+"use strict";
+// Countdown Init Init
+$(document).ready(function () {
+
+    let countdown_clock = $(".countdown-clock");
+    let data_end_date = countdown_clock.attr("data-end-date");
+    let data_UTC = countdown_clock.attr("data-UTC");
+    let data_end_message = countdown_clock.attr("data-end-message");
+
+    countdown_clock.downCount({
+        date: data_end_date,
+        offset: data_UTC
+    }, function () {
+        alert(data_end_message);
+    });
+
+});
+
+
+/**
+ * downCount: Simple Countdown clock with offset
+ * Author: Sonny T. <hi@sonnyt.com>, sonnyt.com
+ */
+
+(function ($) {
+
+    $.fn.downCount = function (options, callback) {
+        let settings = $.extend({
+            date: null,
+            offset: null
+        }, options);
+
+        // Throw error if date is not set
+        if (!settings.date) {
+            $.error('Date is not defined.');
+        }
+
+        // Throw error if date is set incorectly
+        if (!Date.parse(settings.date)) {
+            $.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
+        }
+
+        // Save container
+        let container = this;
+
+        /**
+         * Change client's local date to match offset timezone
+         * @return {Object} Fixed Date object.
+         */
+        let currentDate = function () {
+            // get client's current date
+            let date = new Date();
+
+            // turn date to utc
+            let utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+
+            // set new Date object
+            let new_date = new Date(utc + (3600000 * settings.offset))
+
+            return new_date;
+        };
+
+        /**
+         * Main downCount function that calculates everything
+         */
+        function countdown() {
+            let target_date = new Date(settings.date), // set target date
+                current_date = currentDate(); // get fixed current date
+
+            // difference of dates
+            let difference = target_date - current_date;
+
+            // if difference is negative than it's pass the target date
+            if (difference < 0) {
+                // stop timer
+                clearInterval(interval);
+
+                if (callback && typeof callback === 'function') callback();
+
+                return;
+            }
+
+            // basic math letiables
+            let _second = 1000,
+                _minute = _second * 60,
+                _hour = _minute * 60,
+                _day = _hour * 24;
+
+            // calculate dates
+            let days = Math.floor(difference / _day),
+                hours = Math.floor((difference % _day) / _hour),
+                minutes = Math.floor((difference % _hour) / _minute),
+                seconds = Math.floor((difference % _minute) / _second);
+
+            // fix dates so that it will show two digets
+            days = (String(days).length >= 2) ? days : '0' + days;
+            hours = (String(hours).length >= 2) ? hours : '0' + hours;
+            minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
+            seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+
+            // based on the date change the refrence wording
+            let ref_days = (days === 1) ? 'day' : 'days',
+                ref_hours = (hours === 1) ? 'hour' : 'hours',
+                ref_minutes = (minutes === 1) ? 'minute' : 'minutes',
+                ref_seconds = (seconds === 1) ? 'second' : 'seconds';
+
+            // set to DOM
+            container.find('.days').text(days);
+            container.find('.hours').text(hours);
+            container.find('.minutes').text(minutes);
+            container.find('.seconds').text(seconds);
+
+            container.find('.days_ref').text(ref_days);
+            container.find('.hours_ref').text(ref_hours);
+            container.find('.minutes_ref').text(ref_minutes);
+            container.find('.seconds_ref').text(ref_seconds);
+        };
+
+        // start
+        let interval = setInterval(countdown, 1000);
+    };
+
+})(jQuery);
